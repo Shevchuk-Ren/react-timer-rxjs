3e2f0e1d0649d94f77d8b12edfde9f4084786219
Shevchuk-Ren <shevchuk_re@ukr.net>
2021-09-10 00:14:48 +0300

    refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,59 +8,36 @@ import Buttons from '../Buttons';
 import './App.css';
 
 function App() {
-  // const interval = useRef(null);
   const [time, setTime] = useState(0);
   const [timerOn, setTimerOn] = useState(false);
   const [counter, setCounter] = useState(0);
-  // const [time, setTime] = useState({ ms: 0, s: 0, m: 0, h: 0 });
 
-  const seconds = interval(1000).pipe(
+  const seconds = interval(10).pipe(
     startWith(time),
     scan(time => time + 1),
   );
 
   useEffect(() => {
     if (timerOn) {
-      console.log(time, `use`);
-      // interval.current = interval(1000)
       const sec = seconds.subscribe(setTime);
-      // const sec = interval.subscribe(setTime);
       return () => sec.unsubscribe();
     }
-  }, [timerOn]);
-
-  // useEffect(() => {
-  //   if (timerOn) {
-  //     interval.current = setInterval(() => {
-  //       setTime(prevTime => prevTime + 10);
-  //     }, 10);
-  //   } else {
-  //     clearInterval(interval.current);
-  //   }
-  //   return () => clearInterval(interval);
-  // }, [timerOn]);
-
-  // let updatesHours = time.h,
-  //   updatesSec = time.s,
-  //   updatesMin = time.m,
-  //   updatesMs = 0;
+  }, [timerOn, seconds]);
 
   const start = () => {
-    console.log(time);
+    // console.log(time);
     seconds.subscribe(setTimerOn(true));
-    // interval.subscribe(setTimerOn(true));
-    setTimerOn(true);
-    console.log(timerOn);
+    // setTimerOn(true);
+    // console.log(timerOn);
   };
 
   const stop = () => {
-    setTimerOn(false);
+    seconds.subscribe(setTimerOn(false));
     setTime(0);
   };
 
   const wait = () => {
     setCounter(counter + 1);
-    console.log(time, `time`);
 
     setTimeout(() => {
       setCounter(0);
@@ -72,27 +49,23 @@ function App() {
   };
 
   const reset = () => {
+    // setTime(prevTime => prevTime);
+
     seconds.subscribe(setTimerOn(false));
     setTime(0);
-    console.log(time, `ttt`);
+
+    setTimeout(() => {
+      seconds.subscribe(setTimerOn(true));
+    }, 300);
   };
 
-  // const observable = new Observable(function subscribe(subscriber) {
-  //   const id = setInterval(() => {
-  //     subscriber.next('hi')
-  //   }, 1000);
-  // });
-  // observable.subscribe(x => console.log(x));
-  // const foo = new Observable(subscriber => {
-  //   console.log('Hello');
-  //   subscriber.next(42);
-  //   subscriber.next(100); // "return" another value
-  //   subscriber.next(200); // "return" yet another
-  // });
+  let hours = ('0' + Math.floor((time / 3.6e6) % 24)).slice(-2);
+  let min = ('0' + Math.floor((time / 1000) % 60)).slice(-2);
+  let sec = ('0' + Math.floor((time / 100) % 60)).slice(-2);
 
   return (
     <Container>
-      {/* <Timer time={time}></Timer> */}
+      <Timer sec={sec} min={min} hours={hours}></Timer>
 
       <Buttons
         onStart={start}
@@ -101,7 +74,6 @@ function App() {
         onWait={wait}
         status={timerOn}
       ></Buttons>
-      <div>{time}</div>
     </Container>
   );
 }
--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Timer = ({ time }) => {
+const Timer = ({ sec, min, hours }) => {
   return (
     <div>
       <div className="field">
         <span className="value" data-hours>
-          {('0' + Math.floor((time / 3.6e6) % 24)).slice(-2)}:
+          {hours}:
         </span>
         <span className="label">Hours</span>
       </div>
 
       <div className="field">
         <span className="value" data-minutes>
-          {('0' + Math.floor((time / 60000) % 60)).slice(-2)}:
+          {min}:
         </span>
         <span className="label">Minutes</span>
       </div>
 
       <div className="field">
         <span className="value" data-seconds>
-          {('0' + Math.floor((time / 1000) % 60)).slice(-2)}
+          {sec}
         </span>
         <span className="label">Seconds</span>
       </div>
